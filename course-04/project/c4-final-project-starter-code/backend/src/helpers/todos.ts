254d4888a167703f08fdde59222538417473187d
wksentini <ksentini.wael@gmail.com>
2022-10-16 22:17:55 +0100

    implement createTodo lambda

--- course-04/project/c4-final-project-starter-code/backend/src/helpers/todos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/helpers/todos.ts
@@ -1,10 +1,22 @@
-import { TodosAccess } from './todosAcess'
-import { AttachmentUtils } from './attachmentUtils';
+// import { TodosAccess } from './todosAcess'
+// import { AttachmentUtils } from './attachmentUtils';
 import { TodoItem } from '../models/TodoItem'
 import { CreateTodoRequest } from '../requests/CreateTodoRequest'
-import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
-import { createLogger } from '../utils/logger'
+// import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
+// import { createLogger } from '../utils/logger'
 import * as uuid from 'uuid'
-import * as createError from 'http-errors'
+// import * as createError from 'http-errors'
+import { getUserId } from '../lambda/utils';
 
 // TODO: Implement businessLogic
+export function buildTodo(todoRequest: CreateTodoRequest, event): TodoItem{
+    const todo = {
+        todoId: uuid.v4(),
+        createdAt: new Date().toISOString(),
+        userId: getUserId(event),
+        done: false,
+        attachmentUrl: '',
+        ...todoRequest
+    }
+    return todo
+}
